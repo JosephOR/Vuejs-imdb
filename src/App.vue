fde67ae84fbbc82e0e3bc4d0c96574dd8c28c6f7
Josephoreilly <josephoreilly29@hotmail.com>
2018-01-25 09:38:28 +0000

    initial setup of project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+   <h1>{{ greeting }}</h1>
+    <p>{{ message() }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      greeting: 'Welcome to Your Vue.js App'
+    }
+  },
+    methods: {
+  	  message: function() {
+  		return 'Message from method'
+      }
+    }
+}
+</script>
+
+<style lang="scss">
+
+</style>
